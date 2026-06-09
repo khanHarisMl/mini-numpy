a6495793bbce1be9c43ef735ce65d6f80ecf0377
khanHarisMl <wnnk6bznq8@privaterelay.appleid.com>
2026-06-09 12:23:12 -0700

    Implimented test functions for stage 3 Element-wise operations

--- main.py
+++ main.py
@@ -33,9 +33,37 @@ def test_max():
 
 # Stage 3 Test Functions
 
+def test_add():
+    print("Testing Addition: ")
+    arr = MiniArray([1,2,3])
+    arr2 = MiniArray([4,5,6])
+    print(arr.add(arr2))
+    print("Expected output: MiniArray([5, 7, 9])")
+def test_subtract():
+    print("Testing Subtraction: ")
+    arr = MiniArray([1,2,3])
+    arr2 = MiniArray([4,5,6])
+    print(arr2.subtract(arr))
+    print("Expected output: MiniArray([3, 3, 3])")
+def test_multiply():
+    print("Testing Multiplication: ")
+    arr = MiniArray([1,2,3])
+    arr2 = MiniArray([4,5,6])
+    print(arr.multiply(arr2))
+    print("Expected output: MiniArray([4, 10, 18]) ")
+def test_divide():
+    print("Testing Division: ")
+    arr = MiniArray([1,2,3])
+    arr2 = MiniArray([4,5,6])
+    print(arr2.divide(arr))
+    print("Expected output: MiniArray([4.0, 2.5, 2.0])")
 # Main
 test_append()
 test_sum()
 test_mean()
 test_min()
 test_max()
+test_add()
+test_subtract()
+test_multiply()
+test_divide()
